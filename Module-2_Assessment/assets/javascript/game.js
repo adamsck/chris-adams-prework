358905811c0a5ba21c7bebe9f9aba2dd1a2f2c98
Chris Adams <cadams@shakopeeschools.org>
2020-06-02 14:25:56 -0500

    created the word selection tool

---
+++ Module-2_Assessment/assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+//Declare Variables
+  var wordList= ["Dean Butler","Jose Ortiz","Javier Castellano","Joel Rosario","Mike Smith"]
+  var ansWord = math.floor(math.random()*5)
+  var wordtoGuess = wordList[ansWord];
+  var wordLength= wordtoGuess.length;
+
+  //Word Selection Test
+  alert(wordtoGuess); 
