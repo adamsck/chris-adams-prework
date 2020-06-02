3dc97c4cacda058b585b1314fa795f1a85f1b6bf
Chris Adams <cadams@shakopeeschools.org>
2020-06-02 16:49:55 -0500

    added the process for picking a word

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -8,3 +8,24 @@ var wordList = [
   
 ];
 
+//Establish additional variables
+var ansWord; //word the user needs to guess
+var guessingWord = []; //underscores to represent the word
+
+//Starts game choosing from full set words in the wordList
+function initializeGame() {
+
+  // Get a new word
+  ansWord = wordList[Math.floor(Math.random() * wordList.length)].toUpperCase();
+
+  // Get underscores for guessingWord from ansWord making sure that words with spaces get a space instead of a _
+  for (var i=0; i < ansWord.length; i++){
+    if (ansWord[i] === " ") {
+      guessingWord.push(" ")
+    } 
+    else {
+      guessingWord.push("_ "); //added space after _ to avoid the display looking like a solid line
+    }
+  }
+  updateDisplay();
+};
